00fd82b1965d123de3bdb2b192c5d6132bcfc3de
Hugo Tiem <hugotiem@gmail.com>
2021-05-25 22:04:24 +0200

    adding constants + clean home page

---
+++ lib/constants/cyber_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CyberColors {
+  static const Color darkBlue = Color(0xFF241C52);
+  static const Color blue = Color(0xFF37459C);
+  static const Color grey = Color(0xFFD1D1D8);
+  static const Color whiteGrey = Color(0xFFEAEDFF);
+  static const Color yellow = Color(0xFFFFB900);
+  static const Color green = Color(0xFF5FD08D);
+}
---
+++ lib/constants/data.dart
@@ -0,0 +1,69 @@
+class Data {
+  final _data = [
+    {
+      "title": "Master your passwords",
+      "image": "assets/image-of-your-choice.jpg",
+      "type": "Training",
+      "tags": "Identification / Digital identity",
+      'points': 35,
+      "duration": "15min",
+      "modulesCompleted": 2 / 3,
+    },
+    {
+      "title": "Master your passwords",
+      "image": "assets/image-of-your-choice.jpg",
+      "type": "Training",
+      "tags": "Identification / Digital identity",
+      'points': 35,
+      "duration": "15min",
+      "modulesCompleted": 2 / 3,
+    },
+    {
+      "title": "Protect personal data",
+      "image": "assets/image-of-your-choice.jpg",
+      "type": "Training",
+      "tags": "Protection / Gdpr",
+      'points': 35,
+      "duration": "15min",
+      "modulesCompleted": 1 / 4,
+    },
+    {
+      "title": "Harden your social profiles",
+      "image": "assets/image-of-your-choice.jpg",
+      "type": "Checkup",
+      "tags": "Social network / Privacy",
+      'points': 10,
+      "duration": "5min",
+      "modulesCompleted": 0 / 3,
+    },
+    {
+      "title": "Do your part against ransomwares",
+      "image": "assets/image-of-your-choice.jpg",
+      "type": "Checkup",
+      "tags": "Social engineering / Phishing / Vishing",
+      'points': 10,
+      "duration": "5min",
+      "modulesCompleted": 4 / 4,
+    },
+    {
+      "title": "Software updates and installs at home",
+      "image": "assets/image-of-your-choice.jpg",
+      "type": "Quiz",
+      "tags": "Update / Software",
+      'points': 15,
+      "duration": "10min",
+      "modulesCompleted": 2 / 3,
+    },
+    {
+      "title": "Protect your browser at home",
+      "image": "assets/image-of-your-choice.jpg",
+      "type": "Quiz",
+      "tags": "Protection / Update",
+      'points': 15,
+      "duration": "10min",
+      "modulesCompleted": 1 / 4,
+    },
+  ];
+
+  get data => _data;
+}
